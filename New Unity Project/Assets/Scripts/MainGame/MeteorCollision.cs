21b263c16cfbf16393986c9a089c5a47fb465ce5
SenorMooMoo <timothywigginton1@gmail.com>
2018-01-31 18:00:59 -0500

    StartMenu
    Also changed player movement

---
+++ New Unity Project/Assets/Scripts/MainGame/MeteorCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MeteorCollision : MonoBehaviour {
+
+	public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.gameObject.tag == "Player") {
+			SceneManager.LoadScene(0);
+		} else Destroy (gameObject);
+
+		if (collision.gameObject.tag == "Raft") {
+			
+		}
+	}
+}
