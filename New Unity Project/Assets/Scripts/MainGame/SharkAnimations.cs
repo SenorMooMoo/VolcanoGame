97730c8cf67f262bcc3109f570693138dea71597
DrRAAAAAAAGE <35906671+DrRAAAAAAAGE@users.noreply.github.com>
2018-02-01 18:46:44 -0500

    Day 3
    -Animated Shark
    -Can't make it move yet

---
+++ New Unity Project/Assets/Scripts/MainGame/SharkAnimations.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkAnimations : MonoBehaviour {
+
+	Animation SharkoMove;
+	public float SharkAttackTimer;
+	public float AnimationDelay;
+
+
+	void Awake() {
+		SharkoMove = gameObject.GetComponent<Animation> ();
+		SharkAttackTimer = Random.Range (3, 10);
+		SharkoMove.Play ("SharkMoving");
+		AnimationDelay = 300;
+	}
+
+	void Update () {
+		if (Time.time >= SharkAttackTimer) {
+			AnimationDelay -= 1;
+			SharkoMove.Play ("Shark Attacking");
+			print ("In State: Attacking");
+			if (AnimationDelay <= 0) {
+				SharkAttackTimer = (Random.Range (3, 10) + Time.time);
+			}
+		} else {
+			SharkoMove.Play ("SharkMoving");
+			print ("In State: Moving");
+		}
+	}
+}
