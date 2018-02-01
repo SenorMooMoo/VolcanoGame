1efc76d0b7bacf431a510b9e58e8773888f60f59
SenorMooMoo <timothywigginton1@gmail.com>
2018-02-01 18:25:32 -0500

    push

---
+++ New Unity Project/Assets/Scripts/MainGame/PlayeLimits.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+public class PlayeLimits : MonoBehaviour {
+
+
+	
+	public GameObject Player;
+	
+	private Rigidbody rb;
+
+	// Use this for initialization
+
+	void Start () {
+
+		
+rb = Player.GetComponent<Rigidbody> ();
+	}
+
+	
+// Update is called once per frame
+
+	void Update () {
+
+	
+	Vector3 distanceFromRaft = transform.position - Player.transform.position;
+	
+
+	if(distanceFromRaft.x < 4 && distanceFromRaft.x > 0){
+		
+		rb.AddForce(distanceFromRaft.normalized * 10);		
+
+	}
+	if(distanceFromRaft.x < 0 && distanceFromRaft.x > -4){
+		
+		rb.AddForce(distanceFromRaft.normalized * 10);		
+
+	}
+
+	}
+
+}
